4c5f673148213a256e14a6bb01c2b1e861af19d6
Manjit Bedi <dev@manjitbedi.com>
2023-04-20 16:00:14 -0700

    paused work on building a popup custom UI component using UI toolkit
    It was taking too much time; I should come back to it sometime; I need to analyze how to best achieve this.

--- Assets/scripts/ScalePopup.cs
+++ Assets/scripts/ScalePopup.cs
@@ -3,25 +3,73 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class ScalePopup : VisualElement
+// Not being used at this time
+namespace CustomControls
 {
-    public new class UxmlFactory : UxmlFactory<ScalePopup> { }
 
-    private const string styleResource = "ColorPanel";
-
-    public ScalePopup()
+    /// <summary>
+    /// Not being used at this time
+    /// </summary>
+    public class ColorPopup : VisualElement
     {
-        // load a style sheet
-        styleSheets.Add(Resources.Load<StyleSheet>(styleResource));
-        VisualElement panel = new VisualElement();
+        public new class UxmlFactory : UxmlFactory<ColorPopup> { }
 
-        panel.style.width = 200;
-        panel.style.height = 100;
+        private const string styleResource = "ColorPanel";
 
-        // apply the style sheet to this custom component - note the name;
-        // this is definied in the uss.
-        panel.AddToClassList("default_panel");
+        public ColorPopup()
+        {
+            // load a style sheet
+            styleSheets.Add(Resources.Load<StyleSheet>(styleResource));
+            VisualElement panel = new VisualElement();
 
-        hierarchy.Add(panel);
+            panel.style.width = 200;
+            panel.style.height = 100;
+
+            // apply the style sheet to this custom component - note the name;
+            // this is definied in the uss.
+            panel.AddToClassList("default_panel");
+
+            hierarchy.Add(panel);
+        }
+    }
+
+    /// <summary>
+    /// Displays a popup panel with some buttons to adjust the scale of the displayed mesh.
+    /// Not being used at this time.
+    /// </summary>
+    public class ScaleMeshPopup : VisualElement
+    {
+        public new class UxmlFactory : UxmlFactory<ScaleMeshPopup> { }
+
+        private const string styleResource = "PopupPanel";
+
+        public ScaleMeshPopup()
+        {
+            VisualElement window = new VisualElement();
+            hierarchy.Add(window);
+
+            // load a style sheet
+            styleSheets.Add(Resources.Load<StyleSheet>(styleResource));
+            window.AddToClassList("popup_container");
+
+            // we are creating two visual elements
+            // - the text prompt
+            // - the controls buttons
+            VisualElement horizontalContainertText = new VisualElement();
+            Label promptLabel = new Label();
+            promptLabel.text = "Scale the mesh.";
+            horizontalContainertText.Add(promptLabel);
+
+            VisualElement horizontalContainerButtons = new VisualElement();
+            Button increaseButton = new Button() { text = "+" };
+            Button decreaseButton = new Button() { text = "-" };
+            Button dismissButton = new Button() { text = "dismiss" };
+            horizontalContainerButtons.Add(increaseButton);
+            horizontalContainerButtons.Add(decreaseButton);
+            horizontalContainerButtons.Add(dismissButton);
+
+            window.Add(horizontalContainertText);
+            window.Add(horizontalContainerButtons);
+        }
     }
 }
--- Assets/scripts/controls.cs
+++ Assets/scripts/controls.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using CustomControls;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class controls : MonoBehaviour
 {
+    GameObject meshGameObject;
+
+    [SerializeField]
+    float maxScale = 2.0f;
+
+    [SerializeField]
+    float minScale = 0.25f;
+
     void Awake()
     {
         ConfigureUI();
+    }
+
+    private void Start()
+    {
+
     }
 
     private void ConfigureUI()
@@ -15,13 +29,50 @@ public class controls : MonoBehaviour
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
         VisualElement buttonsContainer = root.Q<VisualElement>("buttons-container");
         //  Get references to the buttons in the UI document.
-        Button button1 = buttonsContainer.Q<Button>("scale");
-        Button button2 = buttonsContainer.Q<Button>("action");
+        Button button1 = buttonsContainer.Q<Button>("scale-up");
+        Button button2 = buttonsContainer.Q<Button>("scale-down");
+        Button button3 = buttonsContainer.Q<Button>("action");
 
-        button1.clicked += () => Debug.Log("action 1");
-        button2.clicked += () => Debug.Log("action 2");
+        button1.clicked += () => ScaleUp();
+        button2.clicked += () => ScaleDown();
+        button3.clicked += () => Debug.Log("action 3 - coming soon");
 
         // for testing purposes
-        // root.Add(new ScalePopup());
+        root.Add(new ScaleMeshPopup());
     }
+
+    #region button actions
+
+    private void ScaleUp()
+    {
+        Debug.Log("action 1");
+        if (meshGameObject == null)
+        {
+            meshGameObject = GameObject.FindGameObjectWithTag("ar-game-object");
+        }
+
+        var currentScale = meshGameObject.transform.localScale.x;
+        if (currentScale < maxScale)
+        {
+            meshGameObject.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+        }
+
+    }
+
+    private void ScaleDown()
+    {
+        Debug.Log("action 2");
+        if (meshGameObject == null)
+        {
+            meshGameObject = GameObject.FindGameObjectWithTag("ar-game-object");
+        }
+
+        var currentScale = meshGameObject.transform.localScale.x;
+        if (currentScale > minScale)
+        {
+            meshGameObject.transform.localScale -= new Vector3(0.1f, 0.1f, 0.1f);
+        }
+    }
+
+    #endregion
 }
